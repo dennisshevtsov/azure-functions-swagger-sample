ccba3fbc650abc2a456281095320ebd4d81f7d4d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-03 01:11:32 +0300

    create todo list

--- AzureFunctionsSwaggerSample.Api/Services/TodoService.cs
+++ AzureFunctionsSwaggerSample.Api/Services/TodoService.cs
@@ -5,13 +5,24 @@
 namespace AzureFunctionsSwaggerSample.Api.Services
 {
   using System;
+  using System.Collections.Generic;
   using System.Threading;
   using System.Threading.Tasks;
 
+  using AzureFunctionsSwaggerSample.Api.Documents;
   using AzureFunctionsSwaggerSample.Api.Dtos;
 
   public sealed class TodoService : ITodoService
   {
+    private readonly IDictionary<Guid, TodoListDocument> _todoListDictionary;
+    private readonly IDictionary<Guid, IList<TodoListTaskDocument>> _todoListTaskDictionary;
+
+    public TodoService()
+    {
+      _todoListDictionary = new Dictionary<Guid, TodoListDocument>();
+      _todoListTaskDictionary = new Dictionary<Guid, IList<TodoListTaskDocument>>();
+    }
+
     public Task<GetTodoListResponseDto> GetTodoListAsync(Guid todoListId, CancellationToken cancellationToken)
     {
       throw new NotImplementedException();
@@ -24,7 +35,25 @@ namespace AzureFunctionsSwaggerSample.Api.Services
 
     public Task<CreateTodoListResponseDto> CreateTodoListAsync(CreateTodoListRequestDto command, CancellationToken cancellationToken)
     {
-      throw new NotImplementedException();
+      var todoListId = Guid.NewGuid();
+      var totoListTasks = new List<TodoListTaskDocument>();
+      var todoListDocument = new TodoListDocument
+      {
+        TodoListId = todoListId,
+        Title = command.Title,
+        Description = command.Description,
+        Tasks = totoListTasks,
+      };
+
+      _todoListDictionary.Add(todoListId, todoListDocument);
+      _todoListTaskDictionary.Add(todoListId, totoListTasks);
+
+      var responseDto = new CreateTodoListResponseDto
+      {
+        TodoListId = todoListId,
+      };
+
+      return Task.FromResult(responseDto);
     }
 
     public Task UpdateProductAsync(UpdateTodoListRequestDto command, CancellationToken cancellationToken)
