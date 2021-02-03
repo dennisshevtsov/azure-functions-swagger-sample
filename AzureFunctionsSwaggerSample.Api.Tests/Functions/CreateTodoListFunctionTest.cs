103cf97a8338090636abf02fe25a0ce04b06d60f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-04 00:31:04 +0300

    test start

---
+++ AzureFunctionsSwaggerSample.Api.Tests/Functions/CreateTodoListFunctionTest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License, Version 2.0.
+// See LICENSE.txt in the project root for license information.
+
+namespace AzureFunctionsSwaggerSample.Api.Tests.Functions
+{
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public sealed class CreateTodoListFunctionTest
+  {
+    [TestInitialize]
+    public void Initialize()
+    {
+    }
+
+    [TestMethod]
+    public void Test()
+    {
+    }
+  }
+}
