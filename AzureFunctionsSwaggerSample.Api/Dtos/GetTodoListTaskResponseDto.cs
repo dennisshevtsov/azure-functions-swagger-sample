e8de91331015a6cfacae7586120f0f99bcfb7991
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-09 17:18:20 +0300

    remove unused code; add prop names

--- AzureFunctionsSwaggerSample.Api/Dtos/GetTodoListTaskResponseDto.cs
+++ AzureFunctionsSwaggerSample.Api/Dtos/GetTodoListTaskResponseDto.cs
@@ -5,23 +5,29 @@
 namespace AzureFunctionsSwaggerSample.Api.Dtos
 {
   using System;
+  using System.Text.Json.Serialization;
 
   /// <summary>Represents detail of a TODO list task.</summary>
   public sealed class GetTodoListTaskResponseDto
   {
     /// <summary>Gets/sets a value that represents an ID of a TODO list task.</summary>
+    [JsonPropertyName("taskId")]
     public Guid TaskId { get; set; }
 
     /// <summary>Gets/sets a value that represents a title of a TODO list task.</summary>
+    [JsonPropertyName("title")]
     public string Title { get; set; }
 
     /// <summary>Gets/sets a value that represents a description of a TODO list task.</summary>
+    [JsonPropertyName("description")]
     public string Description { get; set; }
 
     /// <summary>Gets/sets a value that represents a deadline of a TODO list task.</summary>
+    [JsonPropertyName("deadline")]
     public DateTime Deadline { get; set; }
 
     /// <summary>Gets/sets a value that indicates if a TODO list is completed.</summary>
+    [JsonPropertyName("completed")]
     public bool Completed { get; set; }
   }
 }
