a55a1811d3cdde5da673637e8a4ef7d4df42d1f2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-09 17:28:17 +0300

    remove srv usings

--- AzureFunctionsSwaggerSample.Api/Functions/CreateTodoListFunction.cs
+++ AzureFunctionsSwaggerSample.Api/Functions/CreateTodoListFunction.cs
@@ -18,17 +18,13 @@ namespace AzureFunctionsSwaggerSample.Api.Functions
   /// <summary>Provides a method to handle an HTTP request.</summary>
   public sealed class CreateTodoListFunction
   {
-    private readonly ITodoService _todoService;
     private readonly ISerializationService _serializationService;
 
     /// <summary>Initializes a new instance of the <see cref="AzureFunctionsSwaggerSample.Api.Functions.CreateTodoListFunction"/> class.</summary>
-    /// <param name="todoService">An object that provides a simple API to operate within the TODO list domain.</param>
     /// <param name="serializationService">An object that provides a simple API to serialize/deserialize an object.</param>
     public CreateTodoListFunction(
-      ITodoService todoService,
       ISerializationService serializationService)
     {
-      _todoService = todoService ?? throw new ArgumentNullException(nameof(todoService));
       _serializationService = serializationService ?? throw new ArgumentNullException(nameof(serializationService));
     }
 
--- AzureFunctionsSwaggerSample.Api/Functions/UpdateTodoListFunction.cs
+++ AzureFunctionsSwaggerSample.Api/Functions/UpdateTodoListFunction.cs
@@ -18,17 +18,13 @@ namespace AzureFunctionsSwaggerSample.Api.Functions
   /// <summary>Provides a method to handle an HTTP request.</summary>
   public sealed class UpdateTodoListFunction
   {
-    private readonly ITodoService _todoService;
     private readonly ISerializationService _serializationService;
 
     /// <summary>Initializes a new instance of the <see cref="AzureFunctionsSwaggerSample.Api.Functions.UpdateTodoListFunction"/> class.</summary>
-    /// <param name="todoService">An object that provides a simple API to operate within the TODO list domain.</param>
     /// <param name="serializationService">An object that provides a simple API to serialize/deserialize an object.</param>
     public UpdateTodoListFunction(
-      ITodoService todoService,
       ISerializationService serializationService)
     {
-      _todoService = todoService ?? throw new ArgumentNullException(nameof(todoService));
       _serializationService = serializationService ?? throw new ArgumentNullException(nameof(serializationService));
     }
 
