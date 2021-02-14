26a5f84634f17578391a2b002aaef213799830c2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-02-15 00:14:13 +0300

    fix license header

--- AzureFunctionsSwaggerSample.Api/Documents/TodoListTaskDocument.cs
+++ AzureFunctionsSwaggerSample.Api/Documents/TodoListTaskDocument.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
-// Licensed under the MIT License, Version 2.0.
+// Licensed under the MIT License.
 // See LICENSE.txt in the project root for license information.
 
 namespace AzureFunctionsSwaggerSample.Api.Documents
